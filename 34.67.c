b7134e7b81dda8d7879ee5c40e4cbd345b7e6e13
SaumyaSinha <saumya.23646@stu.upes.ac.in>
2025-11-22 12:38:20 +0530

    day 34

---
+++ 34.67.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() 
+{
+int arr[100], n, pos, num, i;
+printf("Enter number of elements: ");
+scanf("%d", &n);
+printf("Enter %d elements:\n", n);
+for (i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+printf("Enter position to insert (1 to %d): ", n+1);
+scanf("%d", &pos);
+if (pos < 1 || pos > n + 1) 
+    {
+        printf("Invalid position!\n");
+        return 1;
+    }
+printf("Enter number to insert: ");
+scanf("%d", &num);
+for (i = n; i >= pos; i--) 
+    {
+        arr[i] = arr[i-1];
+    }
+arr[pos-1] = num;
+n++;
+printf("Array after insertion:\n");
+for (i = 0; i < n; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+printf("\n");
+return 0;
+}
+
